7874788269d7c3d5aaa10967e7593620d062f08c
Jacob DeLong <62762261+jmdelong7@users.noreply.github.com>
2024-04-20 11:39:35 -0700

    Create buttons for player selection
    Started UI creation by allowing user to select r/p/s by using buttons.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -13,10 +13,27 @@ function getComputerChoice() {
   }
 }
 
-function getPlayerChoice() {
-  let playerSelection = prompt("Rock, Paper, Scissors?").toLowerCase();
-  return playerSelection;
-}
+const rock = document.querySelector("button.rock");
+const paper = document.querySelector("button.paper");
+const scissors = document.querySelector("button.scissors");
+
+rock.addEventListener("click", () => {
+  const playerSelection = "Rock";
+  const computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
+})
+
+paper.addEventListener("click", () => {
+  const playerSelection = "Paper";
+  const computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
+})
+
+scissors.addEventListener("click", () => {
+  const playerSelection = "Scissors";
+  const computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
+})
 
 function playRound(playerSelection, computerSelection) {
 
@@ -49,31 +66,34 @@ function playRound(playerSelection, computerSelection) {
 
 }
 
-function playGame() {
-  let playerScore = 0;
-  let computerScore = 0;
-  
-  for (let i = 0; i < 5; i++){
-    const computerSelection = getComputerChoice();
-    const playerSelection = getPlayerChoice();
-    game_result = playRound(playerSelection, computerSelection);
-    if (game_result.slice(0, 8) === "You win!") {
-      playerScore += 1
-    } else if (game_result.slice(0, 9) === "You lose!") {
-      computerScore += 1;
-    } 
-    console.log(playRound(playerSelection, computerSelection));
-  }
-  
-  if (playerScore > computerScore) {
-    console.log("You win the whole game! You won the most rounds out of 5!")
-  } else if (computerScore > playerScore) {
-    console.log("You lost the whole game! You lost the most rounds out of 5!")
-  } else {
-    console.log("Out of 5 games, you and the computer tied!")
-  }
-}
 
-playGame()
+
+
+// function playGame() {
+//   let playerScore = 0;
+//   let computerScore = 0;
+  
+//   for (let i = 0; i < 5; i++){
+//     const computerSelection = getComputerChoice();
+//     const playerSelection = getPlayerChoice();
+//     game_result = playRound(playerSelection, computerSelection);
+//     if (game_result.slice(0, 8) === "You win!") {
+//       playerScore += 1
+//     } else if (game_result.slice(0, 9) === "You lose!") {
+//       computerScore += 1;
+//     } 
+//     console.log(playRound(playerSelection, computerSelection));
+//   }
+  
+//   if (playerScore > computerScore) {
+//     console.log("You win the whole game! You won the most rounds out of 5!")
+//   } else if (computerScore > playerScore) {
+//     console.log("You lost the whole game! You lost the most rounds out of 5!")
+//   } else {
+//     console.log("Out of 5 games, you and the computer tied!")
+//   }
+// }
+
+// playGame()
 
 
